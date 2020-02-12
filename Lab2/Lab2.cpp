cac154d7b1cf976ea468b5d63d03c24d132a44b0
Jacob Duncan <35266067+jacobduncan00@users.noreply.github.com>
2020-02-12 12:17:39 -0500

    new new

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -6,8 +6,16 @@
 #include <iomanip>
 #include <fstream>
 
-int quickComparisons;
-int mergeComparisons;
+/*
+ * Lab 2 by Jacob Duncan at Salisbury University for Dr. Joseph Anderson
+ * This Lab is a test of QuickSort and MergeSort, almost identical to 
+ * Lab 1, however using two different sorts. This lab will test QuickSort and
+ * MergeSort with values from 100-100000 and record the number of comparisons
+ * the sort made, as well as the time it took for the sort to complete
+*/
+
+int quickComparisons; // global variable / counter for QuickSort comparisons
+int mergeComparisons; // gloval variable / counter for MergeSort comparisons
 
 void resetFiles(){ // function that clears the files in order to write only the current run
   std::ofstream quick;
@@ -18,13 +26,13 @@ void resetFiles(){ // function that clears the files in order to write only the 
   merge.close();
 }
 
-void swap(int &a, int &b){
+void swap(int &a, int &b){ // swap function that swaps two elements of an array
     int temp = a;
     a = b;
     b = temp;
 }
 
-void genRandomArray(int* arr, int n){
+void genRandomArray(int* arr, int n){ // inserts random numbers into array
     srand(time(NULL));
     for (int i = 0; i < n+1; i++){
         int random = (rand()%n);
@@ -32,7 +40,7 @@ void genRandomArray(int* arr, int n){
     }
 }
 
-void genBackwardsArray(int* arr, int n){
+void genBackwardsArray(int* arr, int n){ // inserts descended numbers into array
 	srand(time(NULL));
 	for(int i = 0; i < n; i++){
 		arr[i] = (rand()%n);
@@ -50,14 +58,14 @@ void genBackwardsArray(int* arr, int n){
 	}
 }
 
-void genSortedArray(int* arr, int n){
+void genSortedArray(int* arr, int n){ // inserts ascended numbers into array
 	srand(time(NULL));
 	for(int i = 0; i < n; i++){
 		arr[i] = i;
 	}
 }
 
-void genDuplicatedElementArray(int* arr, int n){
+void genDuplicatedElementArray(int* arr, int n){ // inserts duplicated numbers into array
     srand(time(NULL));
     for(int i = 0; i < n; i++){
         if(i < n/25){
@@ -72,7 +80,7 @@ void genDuplicatedElementArray(int* arr, int n){
 	}
 }
 
-int partition(int* arr, int start, int end){
+int partition(int* arr, int start, int end){ // function that splits the array into two
     int pivot = arr[start];
     int loc = start + 1;
     int swaps = 0;
@@ -88,7 +96,7 @@ int partition(int* arr, int start, int end){
 }
 
 void quickSort(int *arr, int start, int end)
-{
+{ // actually does the QuickSort
   int pivot;
   int swaps = 0;
   if (start <= end - 1)
@@ -102,44 +110,44 @@ void quickSort(int *arr, int start, int end)
 void merge(int *arr, int left, int mid, int right){
   int leftSize = mid - left + 1;
   int rightSize = right - mid;
-  int Left[leftSize], Right[rightSize];
+  int Left[leftSize], Right[rightSize]; // make two new arrays Left and Right
   int i, j, k;
   for(i = 0; i < leftSize; i++){
-    Left[i] = arr[left + i];
+    Left[i] = arr[left + i]; // creates index's for the Left array
   }
   for(j = 0; j < rightSize; j++){
-    Right[j] = arr[mid+j+1];
+    Right[j] = arr[mid+j+1]; // creates index's for the Right array
   }
   i=0, j=0;
   for(k = left; (i < leftSize) && (j < rightSize); k++){
-    if(Left[i] < Right[j]){
+    if(Left[i] < Right[j]){ // if the left is less than the right
       mergeComparisons++;
-      arr[k] = Left[i++];
+      arr[k] = Left[i++]; // add that value to the array
     }
     else{
       mergeComparisons++;
-      arr[k] = Right[j++];
+      arr[k] = Right[j++]; // else add the right value to the array
     }
   }
   while(i < leftSize){
-    arr[k++] = Left[i++];
+    arr[k++] = Left[i++]; // increase size as needed
   }
   while(j < rightSize){
-    arr[k++] = Right[j++];
+    arr[k++] = Right[j++]; // increase size as needed
   }
 }
 
 void mergeSort(int *arr, int left, int right){
   int mid;
   if(left < right){
-    mid = (left+right)/2;
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
-    merge(arr, left, mid, right);
+    mid = (left+right)/2; // get "pivot" or mid of array
+    mergeSort(arr, left, mid); // call QuickSort on the left
+    mergeSort(arr, mid + 1, right); // call QuickSort on the right
+    merge(arr, left, mid, right); // merge the two back together
   }
 }
 
-void testSort(void(*sort)(int*, int, int), int* arr, int s, int e){
+void testSort(void(*sort)(int*, int, int), int* arr, int s, int e){ // test sort function that calculates time each sort takes as well as prints out the number of comparisons each sort took.
     std::ofstream qo;
     qo.open("quick.dat", std::ios_base::app);
     std::ofstream mo;
@@ -160,18 +168,18 @@ void testSort(void(*sort)(int*, int, int), int* arr, int s, int e){
       mo.close();
       std::cout << "-> Comparisons: " << mergeComparisons << std::endl;
     }
-    quickComparisons = 0;
+    quickComparisons = 0; // set the comparisons back to zero
     mergeComparisons = 0;
 }
 
-void printArray(int *arr, int end){
+void printArray(int *arr, int end){ // prints the array, not used, just as an aid to make sure it is sorted
     for(int i = 0; i < end; i++){
         std::cout << arr[i] << " ";
     }
     std::cout << "\n";
 }
 
-void isSorted(int* arr, int n){
+void isSorted(int* arr, int n){ // function that determines if an array is sorted or not
 	if (n == 0){
 		std::cout << "ERROR: Array has no contents" << std::endl;
 		std::cout << std::endl;
@@ -199,19 +207,6 @@ int main(){
     int it = 100;
     int *arr = new int[it];
     void(*sort)(int*, int, int);
-
-    std::cout << std::endl;
-    std::cout << "===================" << std::endl;
-    std::cout << " Partition Testing " << std::endl;
-    std::cout << "===================" << std::endl;
-    int array[] = {4, 9, 8, 2, 3};
-    printArray(array, 5);
-    int p = partition(array, 0, 5);
-    std::cout << "Pivot Location: " << p << std::endl;
-    std::cout << "Pivot Value: " << array[p] << std::endl;
-    printArray(array, 5);
-    std::cout << std::endl;
-
 
       for(int i = 0; i < 25; i++){
         if(i < 12){
