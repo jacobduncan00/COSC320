a1137bd3d3906d585ee8ade69b64ead63e208d6f
Jacob Duncan <35266067+jacobduncan00@users.noreply.github.com>
2020-02-21 18:43:15 -0500

    Done

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "matrix.h"
 
 int main(){
@@ -39,7 +40,7 @@ int main(){
 
     Matrix G;
     G.triangularMatrix(false);
-    std::cout << "\e[1mTriangular Matrix Upper\e[0m" << std::endl;
+    std::cout << "\e[1mTriangular Matrix Lower\e[0m" << std::endl;
     std::cout << std::endl;
     G.printMatrix();
 
@@ -53,5 +54,70 @@ int main(){
     std::cout << "\e[1mVector Matrix\e[0m" << std::endl;
     std::cout << std::endl;
     I.printMatrix();
+
+    std::cout << "\e[1mMultiplication Time\e[0m" << std::endl;
+    std::cout << "\e[1m===================\e[0m" << std::endl;
+    std::cout << std::endl;
+    long unsigned int matrixSizes[6] = {500, 600, 700, 800, 900, 1000};
+    for(int i = 0; i < 6; i++){
+        Matrix *gen = new Matrix(matrixSizes[i], matrixSizes[i]);
+        Matrix *gen2 = new Matrix(matrixSizes[i], matrixSizes[i]);
+        std::cout << "Type: Random" << std::endl;
+        std::cout << "Size: " << matrixSizes[i] << "x" << matrixSizes[i] << std::endl;
+        auto timeStart = std::chrono::system_clock::now();
+        Matrix gen3 = *gen * *gen2;
+        auto timeEnd = std::chrono::system_clock::now();
+        std::chrono::duration<double>elapsed_time2 = timeEnd-timeStart;
+        std::cout << "Time: " << elapsed_time2.count() << "s" << std::endl;
+        std::cout << std::endl;
+    }
+    std::cout << "\e[1m=============\e[0m" << std::endl;
+    std::cout << std::endl;
+    for(int i = 0; i < 6; i++){
+        Matrix *gen = new Matrix(matrixSizes[i], matrixSizes[i]);
+        Matrix *gen2 = new Matrix(matrixSizes[i], matrixSizes[i]);
+        gen->triangularMatrix(true); // upper 
+        gen2->triangularMatrix(true); // upper
+        std::cout << "Type: Upper Triangular" << std::endl;
+        std::cout << "Size: " << matrixSizes[i] << "x" << matrixSizes[i] << std::endl;
+        auto timeStart = std::chrono::system_clock::now();
+        Matrix gen3 = *gen * *gen2;
+        auto timeEnd = std::chrono::system_clock::now();
+        std::chrono::duration<double>elapsed_time2 = timeEnd-timeStart;
+        std::cout << "Time: " << elapsed_time2.count() << "s" << std::endl;
+        std::cout << std::endl;
+    }
+    std::cout << "\e[1m=============\e[0m" << std::endl;
+    std::cout << std::endl;
+    for(int i = 0; i < 6; i++){
+        Matrix *gen = new Matrix(matrixSizes[i], matrixSizes[i]);
+        Matrix *gen2 = new Matrix(matrixSizes[i], matrixSizes[i]);
+        gen->triangularMatrix(false); // lower
+        gen2->triangularMatrix(false); // lower
+        std::cout << "Type: Lower Triangular" << std::endl;
+        std::cout << "Size: " << matrixSizes[i] << "x" << matrixSizes[i] << std::endl;
+        auto timeStart = std::chrono::system_clock::now();
+        Matrix gen3 = *gen * *gen2;
+        auto timeEnd = std::chrono::system_clock::now();
+        std::chrono::duration<double>elapsed_time2 = timeEnd-timeStart;
+        std::cout << "Time: " << elapsed_time2.count() << "s" << std::endl;
+        std::cout << std::endl;
+    }
+    std::cout << "\e[1m=============\e[0m" << std::endl;
+    std::cout << std::endl;
+    for(int i = 0; i < 6; i++){
+        Matrix *gen = new Matrix(matrixSizes[i], matrixSizes[i]);
+        Matrix *gen2 = new Matrix(matrixSizes[i], matrixSizes[i]);
+        gen->identityMatrix(); // upper 
+        gen2->identityMatrix(); // upper
+        std::cout << "Type: Identity" << std::endl;
+        std::cout << "Size: " << matrixSizes[i] << "x" << matrixSizes[i] << std::endl;
+        auto timeStart = std::chrono::system_clock::now();
+        Matrix gen3 = *gen * *gen2;
+        auto timeEnd = std::chrono::system_clock::now();
+        std::chrono::duration<double>elapsed_time2 = timeEnd-timeStart;
+        std::cout << "Time: " << elapsed_time2.count() << "s" << std::endl;
+        std::cout << std::endl;
+    }
     return 0;
 }
