fba21f33114e82ac1c263b1ada047494b627cb1d
jacobduncan00 <jacobduncan00@yahoo.com>
2020-03-08 20:58:37 -0400

    new

--- NewProject1/Matrix.h
+++ NewProject1/Matrix.h
@@ -9,33 +9,33 @@ class Matrix{
         long int rows;
         long int cols;
     public:
-        Matrix(); // works
-        Matrix(long int, long int); // work
-        Matrix(const Matrix&); // works
-        ~Matrix(); 
-        Matrix fillMatrix(); // works
-        Matrix fillMatrix2();
-        Matrix fillMatrix3();
-        void insert(long int, long int, double);
-        double getVal(int, int);
-        void determinant();
-        bool isSymmetric();
-        Matrix transpose();
-        Matrix padMatrix(int);
-        Matrix inverse();
-        void twoDRegression(std::string); // takes in fileName by argv in main
-        Matrix identityMatrix(); // works
-        void diagonalMatrix(); // works
-        void triangularMatrix(bool); // works
-        void inversePrint();
-        void printMatrix(); // works
-        // void printByCols(int);
-        Matrix& operator= (const Matrix&); // works
-        friend std::ostream& operator<< (std::ostream& os, const Matrix& matrix); // works
-        friend Matrix operator+ (const Matrix& matrixa, const Matrix& matrixb);
-        friend Matrix operator- (const Matrix& matrixa, const Matrix& matrixb);
-        friend Matrix operator* (const Matrix& matrixa, const Matrix& matrixb);
-        friend Matrix operator* (const Matrix& matrixa, const double& c);
+        Matrix(); // Default Constructor
+        Matrix(long int, long int); // Overloaded Constructor takes Rows & Cols
+        Matrix(const Matrix&); // Copy Constructor
+        ~Matrix(); // Destructor
+        Matrix& operator= (const Matrix&); // Overloaded Assignment Operator
+        
+        Matrix fillMatrix(); // Fill Matrix Test #1         used in driver
+        Matrix fillMatrix2(); // Fill Matrix Test #2        used in driver
+        Matrix fillMatrix3(); // Fill Matrix Test #3        used in driver
+        void insertNum(int, int, double); // inserts a double into the matrix at a specified location
+        double getVal(int, int); // returns a double in the matrix at a specified location
+        bool isSymmetric(); // Checks to see if Matrix is symmetrical
+        Matrix transpose(); // Returns a tranposed Matrix of the one called upon
+        Matrix padMatrix(int); // Pads the Matrix called upon to be a 2^n x 2^n
+        Matrix inverse(); // Returns the inverse of a given Matrix
+        Matrix identityMatrix(); // Returns the Identity Matrix of a given Matrix
+        void diagonalMatrix(); // Creates the Diagonal Matrix of a given Matrix
+        void triangularMatrix(bool); // Creates the Triangular Matrix of a given Matrix
+        void inversePrint(); // Due to floating point errors, print that rounds the product of A * A inverse 
+        void printMatrix(); // Prints out a Matrix
+        int printCounter(); // Prints out the arithmetic counter for certain algorithms
+        
+        friend std::ostream& operator<< (std::ostream& os, const Matrix& matrix); // Overloaded output operator, used to print Matrices
+        friend Matrix operator+ (const Matrix& matrixa, const Matrix& matrixb); // Overloaded addition operator, used to add Matrices
+        friend Matrix operator- (const Matrix& matrixa, const Matrix& matrixb); // Overloaded subtraction operator, used to subtract Matrices
+        friend Matrix operator* (const Matrix& matrixa, const Matrix& matrixb); // Overloaded multiplication operator, used to multiply Matrices
+        friend Matrix operator* (const Matrix& matrixa, const double& c); // Overloaded multiplication operator, used to multiply matrices by a scalar
 };
 
 #endif
---
+++ NewProject1/iomodel.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "Matrix.h"
+
+Matrix calcDemand(Matrix& in, Matrix& dem){
+    Matrix id = in.identityMatrix();  
+    Matrix rtn = ((id - in).inverse()) * (dem);
+    return rtn;
+}
+
+int main(int argc, char** argv){
+    if(argc != 2){
+        std::cout <<"You only input 1 parameter, this program takes 2!" << std::endl;
+        return 0;
+    }
+    std::ifstream inFile;
+    try{
+        inFile.open(argv[1]);
+    } catch (...){
+        std::perror("ERROR: Input file not opened.");
+        return 0;
+    }
+
+    int dimensions = 0;
+    std::string firstLevelNames[50];
+    while(inFile.peek() != EOF){
+        std::cout << "First Level: " << std::endl;
+        std::cout << std::endl;
+        for(int i = 0; inFile.peek() != '-'; i++){
+            std::getline(inFile, firstLevelNames[i]);
+            std::cout << firstLevelNames[i] << std::endl;
+            dimensions++;
+        }
+        std::cout << std::endl;
+        std::cout << "Second Level: " << std::endl;
+        std::cout << std::endl;
+        inFile.ignore(3, '\n');
+        Matrix fileMatrix(dimensions, dimensions);
+        double insertNum = 0;
+        for(int i = 0; i < dimensions; i++){
+            for(int j = 0; j < dimensions; j++){
+                inFile >> insertNum;
+                fileMatrix.insertNum(i, j, insertNum);
+            }
+        }
+        std::cout << fileMatrix << std::endl;
+        std::cout << "Third Level: " << std::endl;
+        std::cout << std::endl;
+        inFile.ignore(5, '\n');
+        inFile.ignore(5, '\n');
+        Matrix demand(dimensions, 1);
+        double insertNum2 = 0;
+        for(int i = 0; i < dimensions; i++){
+            inFile >> insertNum2;
+            demand.insertNum(i, 0, insertNum2);
+        }
+        std::cout << demand << std::endl;
+        std::cout << "================================" << std::endl;
+        std::cout << "Amount of each producted needed:" << std::endl;
+        Matrix result = calcDemand(fileMatrix, demand); 
+        for(int i = 0; i < dimensions; i++){
+            double matrixReturn = result.getVal(i, 0);
+            std::cout << firstLevelNames[i] << ": " << matrixReturn << " units" << std::endl;
+        }
+        std::cout << "================================" << std::endl;
+        inFile.ignore(5, '\n');
+        inFile.ignore(5, '\n');
+        insertNum = 0;
+        insertNum2 = 0;
+    }
+    inFile.close();
+    return 0;
+}
