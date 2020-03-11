1b6cf26c0724143a5023f5f179449dfbb1a015fb
jacobduncan00 <jacobduncan00@yahoo.com>
2020-03-10 22:43:40 -0400

    doing strassens then done

--- NewProject1/Matrix.cpp
+++ NewProject1/Matrix.cpp
@@ -3,11 +3,14 @@
 #include <math.h>
 #include "Matrix.h"
 
-namespace counter{
+// keeps track of arithmetic operations
+namespace counter{ 
     int count = 0;
 }
 
-Matrix::Matrix(){
+// default constructor forms a new 2D Array "Matrix" 3x3 
+
+Matrix::Matrix(){ 
     mat = new double*[3];
     for(int i = 0; i < 3; i++){
       mat[i] = new double[3];
@@ -15,6 +18,10 @@ Matrix::Matrix(){
     rows = 3;
     cols = 3;
 }
+
+// Overloaded constructor that takes in "rows" and "cols" long integer arguments
+// This constructor will form a matrix given the parameters pass into it
+// Ex. Matrix(4,5) makes a 4x5 Matrix
 
 Matrix::Matrix(long int row, long int col){
   if (row <= 0 || col <= 0){
@@ -29,6 +36,10 @@ Matrix::Matrix(long int row, long int col){
   }
 }
 
+// Copy Constructor that takes in a constant Matrix by and assigns into 
+// to a new Matrix that will be returned from the function
+// Ex. Matrix b(Matrix a), therefore Matrix b would have the same contents
+// as Matrix a
 
 Matrix::Matrix(const Matrix& m){
   rows = m.rows;
@@ -45,12 +56,18 @@ Matrix::Matrix(const Matrix& m){
   }
 }
 
+// Destructor, deallocates the memory assigned by Matrix objects
+// or functions that use the new operator to allocate memory
+
 Matrix::~Matrix(){
   for(long int i = 0; i < this->rows; i++){
     delete[] mat[i];
   }
   delete[] mat;
 }
+
+// Function that fills a 3x3 Matrix with specific values
+// shown below
 
 Matrix Matrix::fillMatrix(){
   Matrix A = Matrix(rows, cols);
@@ -66,6 +83,9 @@ Matrix Matrix::fillMatrix(){
   return A;
 }
 
+// Function that fills a 3x3 Matrix with specific values
+// shown below
+
 Matrix Matrix::fillMatrix2(){
   Matrix A = Matrix(rows, cols);
   A.mat[0][0] = 5.0;
@@ -79,6 +99,9 @@ Matrix Matrix::fillMatrix2(){
 	A.mat[2][2] = 6.0;
   return A;
 }
+
+// Function that fills a 4x4 Matrix with specific values
+// shown below
 
 Matrix Matrix::fillMatrix3(){
   Matrix A = Matrix(rows, cols);
@@ -101,6 +124,10 @@ Matrix Matrix::fillMatrix3(){
   return A;
 }
 
+// Function that overwrites an index in the 2D array/matrix
+// Ex Matrix a;
+// a.insert(0,0,1) replaces the index [0][0] with the number 1=
+
 void Matrix::insertNum(int i, int j, double num){
   if((i < 0 || i > this->rows) || (j < 0 || j > this->cols)){
     throw "ERROR: Indeces are not valid.";
@@ -108,12 +135,27 @@ void Matrix::insertNum(int i, int j, double num){
   this->mat[i][j] = num;
 }
 
+// Function that retreives the value in the 2D array/matrix
+// Ex Matrix a;
+// a.getVal(0,0) returns the number at index [0][0]
+
 double Matrix::getVal(int i, int j){
   if((i < 0 || i > this->rows) || (j < 0 || j > this->cols)){
     throw "ERROR: Indeces are not valid.";
   }
   return this->mat[i][j];
 }
+
+// Function that checks whether a Matrix is symmetrical or not
+// Meaning whether the values reflected across the main diagonal
+// are the same or not.
+// Ex. Matrix A = [1 2 3 
+//                 2 4 5
+//                 3 5 8] is symmetric
+// 
+// where as Matrix B = [1 2 3
+//                      4 5 6
+//                      7 8 9] is not symmetric
 
 bool Matrix::isSymmetric(){
   Matrix a = *this;
@@ -128,12 +170,44 @@ bool Matrix::isSymmetric(){
   return true;
 }
 
+// Function that creates a new Matrix which is the transpose of 
+// the Matrix that called the function.
+// Ex. Matrix A = [1 2 3
+//                 4 5 6
+//                 7 8 9];
+// A.transpose();
+// A now looks like [1 4 7 
+//                   2 5 8 
+//                   3 6 9];
+
+Matrix Matrix::transpose(){ 
+	Matrix transpose(cols, rows);
+	for(long int i = 0; i < rows; i++){
+		for(long int j = 0; j < cols; j++){
+			transpose.mat[j][i] = mat[i][j];
+    }
+  }
+  counter::count++;
+	return transpose;
+}
+
+// Function that pads a Matrix to become a 2^N x 2^N
+// For example Matrix A = [1 2 3
+//                         4 5 6
+//                         7 8 9] needs to be padded to     |
+//                                                          V
+//
+//                                                       [1 2 3 0
+//                                                        4 5 6 0
+//                                                        7 8 9 0 
+//                                                        0 0 0 0] in order to use inverse function 
+
 Matrix Matrix::padMatrix(int d){
 	int size = 0;
-  while(log2(d + size) - (int)log2(d + size) != 0){
+  while(log2(d + size) - (int)log2(d + size) != 0){ // finds the next highest 2^N value "power of 2"
     size++;
   }
-  Matrix rtn(d+size, d+size);
+  Matrix rtn(d+size, d+size); // creates new Matrix with the new padded size
   Matrix id(size, size);
   id.identityMatrix();
   for(long int i = 0; i < rtn.rows; i++){
@@ -153,13 +227,11 @@ Matrix Matrix::padMatrix(int d){
 }
 
 Matrix Matrix::inverse(){
-  // Assuming that "this" is a square (n x n) matrix and is symmetric
-
-  if(rows != cols){
+  if(rows != cols){ // if not a square Matrix
     throw "Not a square matrix!";
   }
 
-  if(rows == 1 || cols == 1){
+  if(rows == 1 || cols == 1){ // if Matrix is a 1x1
     Matrix rtn(1,1);
     if(mat[0][0] != 0){
       rtn.mat[0][0] = 1.0/mat[0][0];
@@ -170,32 +242,32 @@ Matrix Matrix::inverse(){
     return rtn;
   }
 
-  int ogRow = rows;
+  int holdRows = rows;
   if(log2(rows) - (int)log2(rows) != 0){ // check to see if Matrix is a 2^n x 2^n
     Matrix fixCurr = padMatrix(rows);
     Matrix rtn = fixCurr.inverse();
-    Matrix newRtn(ogRow, ogRow);
-    for(long int i = 0; i < ogRow; i++){
-      for(long int j = 0; j < ogRow; j++){
+    Matrix newRtn(holdRows, holdRows);
+    for(long int i = 0; i < holdRows; i++){
+      for(long int j = 0; j < holdRows; j++){
         newRtn.mat[i][j] = rtn.mat[i][j];
       }
     }
     return newRtn;
   }
 
-  if(!isSymmetric()){
-    Matrix temp = *this;
-    Matrix temp2 = temp.transpose();
-    Matrix temp3 = temp2 * temp;
-    Matrix temp4 = temp3.inverse() * temp2;
-    return temp4;
+  if(!isSymmetric()){ // check to see if Matrix is symmetric
+    Matrix op1 = *this; // takes Matrix
+    Matrix op2 = op1.transpose(); // transposes Matrix
+    Matrix op3 = op2 * op1; // multiplies Matrix by its transpose
+    Matrix op4 = op3.inverse() * op2; // inverses that and multiplies by its transpose again
+    return op4;
   }
 
 	long int row2 = rows/2;
 	long int col2 = cols/2;
 
 	Matrix B(row2, col2); // n/2 matrix
-	Matrix C(row2, col2); // n/2 matrix
+	Matrix C(row2, col2); // n/2 matrix           Splitting Matrix into 4 slices
 	Matrix CT(row2, col2); // n/2 matrix
 	Matrix D(row2, col2); // n/2 matrix
 
@@ -217,18 +289,19 @@ Matrix Matrix::inverse(){
 		}
   }
 
-	Matrix BI = (B.inverse()); // recursively call this function
+	Matrix BI = (B.inverse()); // recursively calls this function
 	Matrix W = (C) * (BI);
   Matrix WT = W.transpose();
 	Matrix X = W * CT;
 	Matrix S = D - X;
-	Matrix V = (S.inverse());
+	Matrix V = (S.inverse()); // recursively calls this function
 	Matrix Y = V * (W);
 	Matrix YT = Y.transpose();
 	Matrix T = (YT * -1);
 	Matrix U = (Y * -1);
 	Matrix Z = (WT) * (Y);
 	Matrix R = BI + Z;
+
   Matrix rtnNew(rows, cols);
 
   for(long int i = 0; i < rows; i++){
@@ -250,37 +323,47 @@ Matrix Matrix::inverse(){
 	return rtnNew;
 }
 
+// Function that turns a Matrix into it's respected identity Matrix
+// given its dimensions
+
 void Matrix::identityMatrix(){
   Matrix temp(this->rows, this->cols);
   for(long int i = 0; i < this->rows; i++){
     for(long int j = 0; j < this->cols; j++){
       if(i == j){
-        mat[i][j] = 1;
+        mat[i][j] = 1; // creates 1's down the main diagonal
       }
       else{
-        mat[i][j] = 0;
+        mat[i][j] = 0; // everywhere else is 0's
       }
     }
   }
 }
 
+// Function that turns a Matrix into it's respected diagonal Matrix
+// given its dimensions
+
 void Matrix::diagonalMatrix(){
-  srand(time(NULL));
   for(long int i = 0; i < this->rows; i++){
     this->mat[i][i] = 9;
   }
 }
 
+// Function that turns a Matrix into it's respected triangular Matrix
+// given its dimensions. Takes in a boolean value as a parameter and
+// this determines whether or not it will be a upper or lower triangle
+// if boolean value passed in is true it will be a upper triangle where as if
+// the boolean value passed in is false it will be a lower triangle
+
 void Matrix::triangularMatrix(bool up){
-  srand(time(NULL));
   if(up){
     for(long int i = 0; i < this->rows; i++){
       for(long int j = 0; j < this->rows; j++){
         if(i < j){
-          this->mat[i][j] = 9;
+          this->mat[i][j] = 9; // creates 9's in the upper quadrant of the triangle
         }
         else{
-          this->mat[i][j] = 0;
+          this->mat[i][j] = 0; // else put 0's in the lower quadrant of the triangle
         }
       }
     }
@@ -289,15 +372,21 @@ void Matrix::triangularMatrix(bool up){
     for(long int i = 0; i < this->rows; i++){
       for(long int j = 0; j < this->rows; j++){
         if(i > j){
-          this->mat[i][j] = 9;
+          this->mat[i][j] = 9; // creates 9's in the lower quadrant of the triangle
         }
         else{
-          this->mat[i][j] = 0;
+          this->mat[i][j] = 0; // else put 0's in the upper quadrant of the triangle
         }
       }
     }
   }
 }
+
+// Function that addresses the floating point rounding error in C++
+// This function will round numbers between 0.9 ~ 1 to 1
+// and will round numbers between -.1 ~ 0 to 0
+// I do this because this will truly show that A * A.inverse() is indeed the 
+// identity Matrix
 
 void Matrix::inversePrint(){ 
   for(long int i = 0; i < this->rows; i++){
@@ -305,8 +394,11 @@ void Matrix::inversePrint(){
             std::cout << std::endl;
 	      }
         for(long int j = 0; j < this->cols; j++){
-          if(this->mat[i][j] <= 0 && this->mat[i][j] >= -1){
-            std::cout << std::setw(5) << (int)this->mat[i][j]*(-1) << " ";;
+          if(this->mat[i][j] > 0.9 && this->mat[i][j] < 1){
+            std::cout << std::setw(5) << 1 << " ";;
+          }
+          else if(this->mat[i][j] > -.1 && this->mat[i][j] < 0){
+            std::cout << std::setw(5) << 0 << " ";
           }
           else{
             std::cout << std::setw(5) << (int)this->mat[i][j] << " ";
@@ -316,6 +408,9 @@ void Matrix::inversePrint(){
     std::cout << std::endl;
     std::cout << std::endl;
 }
+
+// Function that prints out a Matrix normally
+// Essentially printing a 2D array
 
 void Matrix::printMatrix(){
     for(long int i = 0; i < this->rows; i++){
@@ -330,24 +425,38 @@ void Matrix::printMatrix(){
     std::cout << std::endl;
 }
 
+// For any given Matrix, this function will print 
+// the number of arithmetical operations that Matrix
+// had applied to itself
+
 int Matrix::printCounter(){
   return counter::count;
 }
 
+// Overloaded assignment operator used for assigning
+// Matrices to other Matrices
+// Ex. Matrix A (5,5);
+// Matrix B (5,5);
+// B = A;
+
 Matrix& Matrix::operator=(const Matrix& m){
   if(this == &m){ // when matrices are already the same
-    return *this;
+    return *this; 
   }
   for(long int i = 0; i < rows; i++){
     delete[] mat[i];
   }
+
   delete[] mat;
   mat = new double*[m.rows];
+
   for(long int i = 0; i < m.rows; i++){
     mat[i] = new double[m.cols];
   }
+
   rows = m.rows;
   cols = m.cols;
+
   for(long int i = 0; i < rows; i++){
     for(long int j = 0; j < cols; j++){
       mat[i][j] = m.mat[i][j];
@@ -357,20 +466,30 @@ Matrix& Matrix::operator=(const Matrix& m){
   return *this;
 }
 
+// Overloaded outstream operator that is used to print Matrices easier
+// Ex. Matrix A (5,5);
+// std::cout << A << std::endl;
+// Will print the content of the Matrix the same way A.printMatrix()
+// would
+
 std::ostream& operator<< (std::ostream& os, const Matrix& matrix){
     for(long int i = 0; i < matrix.rows; i++){
         for(long int k = 0; k < matrix.cols; k++){
             os << matrix.mat[i][k] << " ";
         }
-        std::cout << "\n";
+        std::cout << std::endl;
     }
     return os;
 }
 
+// Overloaded addition operator that is used to add Matrices together
+// Ex. Matrix A (5,5);
+// Matrix B (5,5);
+// Matrix C = A + B;
+
 Matrix operator+ (const Matrix& matrixa, const Matrix& matrixb){
 	if(matrixa.rows != matrixb.rows || matrixa.cols != matrixb.cols){
-		std::cout << "Invalid matrix size combination.";
-		exit (1);
+		throw "Invalid matrix size combination.";
 	}
 
 	Matrix matrixc(matrixa.rows, matrixa.cols);
@@ -383,10 +502,14 @@ Matrix operator+ (const Matrix& matrixa, const Matrix& matrixb){
 	return matrixc;
 }
 
+// Overloaded subtraction operator that is used to subtract Matrices
+// Ex. Matrix A (5,5);
+// Matrix B (5,5);
+// Matrix C = A - B;
+
 Matrix operator- (const Matrix& matrixa, const Matrix& matrixb){
 	if(matrixa.rows!= matrixb.rows|| matrixa.cols != matrixb.cols){
-		std::cout << "Invalid matrix size combination.";
-		exit (1);
+		throw "Invalid matrix size combination.";
 	}
 	Matrix matrixc(matrixa.rows, matrixa.cols);
 	for(long int i = 0; i < matrixa.rows;i++){
@@ -398,10 +521,15 @@ Matrix operator- (const Matrix& matrixa, const Matrix& matrixb){
 	return matrixc;
 }
 
+// Overloaded multiplication operator that is used to multiply Matrices together
+// Ex. Matrix A (5,5);
+// Matrix B (5,5);
+// Matrix C = A * B;
+
+
 Matrix operator* (const Matrix& matrixa, const Matrix& matrixb){
 	if(matrixa.cols != matrixb.rows){
-		std::cout << "Invalid matrix size combination.";
-		exit(1);
+	  throw "Invalid matrix size combination.";
 	}
 	Matrix matrixc(matrixa.rows, matrixb.cols);
 	for(long int i = 0; i < matrixa.rows; i++){
@@ -417,7 +545,12 @@ Matrix operator* (const Matrix& matrixa, const Matrix& matrixb){
 	return matrixc;
 }
 
-Matrix operator* (const Matrix& matrixa, const double& c){ // Scalar Mult.
+// Overloaded addition operator that is used to multiply Matrices by a scalar
+// Ex. Matrix A (5,5);
+// double B = 5;
+// Matrix C = A * B; or Matrix C = A * 5;
+
+Matrix operator* (const Matrix& matrixa, const double& c){ 
 	Matrix matrixb(matrixa.rows, matrixa.cols);
 	for(long int i = 0; i < matrixa.rows; i++){
 		for(long int k = 0; k < matrixa.cols; k++){
@@ -426,15 +559,4 @@ Matrix operator* (const Matrix& matrixa, const double& c){ // Scalar Mult.
   }
   counter::count++;
 	return matrixb;
-}
-
-Matrix Matrix::transpose(){ // Tranpose function
-	Matrix transpose(cols, rows);
-	for(long int i = 0; i < rows; i++){
-		for(long int j = 0; j < cols; j++){
-			transpose.mat[j][i] = mat[i][j];
-    }
-  }
-  counter::count++;
-	return transpose;
 }
--- NewProject1/iomodel.cpp
+++ NewProject1/iomodel.cpp
@@ -3,6 +3,23 @@
 #include <iomanip>
 #include "Matrix.h"
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      
+#define RED     "\033[31m"     
+#define GREEN   "\033[32m"      
+#define YELLOW  "\033[33m"      
+#define BLUE    "\033[34m"      
+#define MAGENTA "\033[35m"      
+#define CYAN    "\033[36m"      
+#define BOLDBLACK   "\033[1m\033[30m"      
+#define BOLDRED     "\033[1m\033[31m"     
+#define BOLDGREEN   "\033[1m\033[32m"      
+#define BOLDYELLOW  "\033[1m\033[33m"      
+#define BOLDBLUE    "\033[1m\033[34m"     
+#define BOLDMAGENTA "\033[1m\033[35m"     
+#define BOLDCYAN    "\033[1m\033[36m"    
+#define BOLDWHITE   "\033[1m\033[37m"   
+
 Matrix calcDemand(Matrix& in, Matrix& dem){
     Matrix id = in;
     id.identityMatrix();
@@ -10,24 +27,42 @@ Matrix calcDemand(Matrix& in, Matrix& dem){
     return rtn;
 }
 
+void welcomeScreen(){
+    std::cout << BOLDRED << "**************************************" << RESET << std::endl;
+    std::cout << BOLDRED << "**************************************" << RESET << std::endl;
+    std::cout << BOLDRED << "**                                  **" << RESET << std::endl;
+    std::cout << BOLDRED << "**             WELCOME              **" << RESET << std::endl;
+    std::cout << BOLDRED << "**        TO YOUR SUPPLY &          **" << RESET << std::endl;
+    std::cout << BOLDRED << "**       DEMAND CALCULATOR BY       **" << RESET << std::endl;
+    std::cout << BOLDRED << "**           JACOB DUNCAN           **" << RESET << std::endl;
+    std::cout << BOLDRED << "**                                  **" << RESET << std::endl;
+    std::cout << BOLDRED << "**************************************" << RESET << std::endl;
+    std::cout << BOLDRED << "**************************************" << RESET << std::endl;
+}
+
 int main(int argc, char** argv){
+
+    welcomeScreen();
+
     if(argc != 2){
         std::cout <<"You only input 1 parameter, this program takes 2!" << std::endl;
         return 0;
     }
+
     std::ifstream inFile;
+
     try{
-        inFile.open(argv[1]);
+        inFile.open(argv[1]); // attempts to open file
     } catch (...){
-        std::perror("ERROR: Input file not opened.");
+        std::perror("ERROR: Input file not opened."); // if cannot be opened print error and end program, cannot run with no data
         return 0;
     }
 
     int dimensions = 0;
-    std::string firstLevelNames[50];
+    std::string firstLevelNames[1000]; // company limit is 1000, please don't go over 1000 Dr.Anderson
     bool flag = false;
     while(inFile.peek() != EOF){
-        std::cout << "First Level: " << std::endl;
+        std::cout << BOLDRED << "First Level: " << RESET << std::endl;
         std::cout << std::endl;
         for(int i = 0; inFile.peek() != '-'; i++){
             std::getline(inFile, firstLevelNames[i]);
@@ -35,7 +70,7 @@ int main(int argc, char** argv){
             dimensions++;
         }
         std::cout << std::endl;
-        std::cout << "Second Level: " << std::endl;
+        std::cout << BOLDRED << "Second Level: " << RESET << std::endl;
         std::cout << std::endl;
         inFile.ignore(3, '\n');
         Matrix fileMatrix(dimensions, dimensions);
@@ -47,7 +82,7 @@ int main(int argc, char** argv){
             }
         }
         std::cout << fileMatrix << std::endl;
-        std::cout << "Third Level: " << std::endl;
+        std::cout << BOLDRED << "Third Level: " << RESET << std::endl;
         std::cout << std::endl;
         inFile.ignore(5, '\n');
         inFile.ignore(5, '\n');
@@ -58,14 +93,20 @@ int main(int argc, char** argv){
             demand.insertNum(i, 0, insertNum2);
         }
         std::cout << demand << std::endl;
-        std::cout << "================================" << std::endl;
+        std::cout << BOLDRED << "================================" << std::endl;
         std::cout << "Amount of each producted needed:" << std::endl;
-        Matrix result = calcDemand(fileMatrix, demand); 
+        Matrix result;
+        try{
+            result = calcDemand(fileMatrix, demand); 
+        }
+        catch(std::string errorMessage){
+            std::cout << errorMessage << std::endl;
+        }
         for(int i = 0; i < dimensions; i++){
             double matrixReturn = result.getVal(i, 0);
-            std::cout << firstLevelNames[i] << ": " << matrixReturn << " units" << std::endl;
+            std::cout << BOLDRED << firstLevelNames[i] << ": " << RESET << matrixReturn << " units" << std::endl;
         }
-        std::cout << "================================" << std::endl;
+        std::cout << BOLDRED << "================================" << RESET << std::endl;
         inFile.ignore(5, '\n');
         inFile.ignore(5, '\n');
         insertNum = 0;
