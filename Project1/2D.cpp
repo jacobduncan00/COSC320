20d392546f08ee109767b4063bedf19436b656ef
Jacob Duncan <35266067+jacobduncan00@users.noreply.github.com>
2020-03-12 18:42:24 -0400

    not needed

--- Project1/2D.cpp
+++
@@ -1,9 +0,0 @@
-#include "matrix.h"
-#include <iostream>
-
-// int argc, char** argv
-int main(int argc, char** argv){
-    std::string fileName = argv[1];
-	Matrix T;
-    T.TwoDRegression(fileName);
-}
--- Project1/3D.cpp
+++
@@ -1,7 +0,0 @@
-#include "matrix.h"
-
-int main(int argc, char** argv){
-  std::string fileName = argv[1];
-  Matrix Z;
-  Z.ThreeDRegression(fileName);
-}
