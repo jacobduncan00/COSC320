09ca24d47b63a4d46b037bb0bb8896852face280
Jacob Duncan <35266067+jacobduncan00@users.noreply.github.com>
2020-03-24 09:01:39 -0400

    new

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -7,6 +7,53 @@ void printHash(size_t);
 size_t hashTest1(std::string);
 size_t hashTest2(std::string);
 
+int main(){
+  std::cout << "---------------------" << std::endl;
+  std::cout << "Testing hashing ints:" << std::endl;
+  std::cout << "---------------------" << std::endl;
+  std::cout << "Int: " << 5 << " | ";
+  printHash(hash(5));
+  std::cout << "Int: " << 100 << " | ";
+  printHash(hash(100));
+  std::cout << "Int: " << 5000 << " | ";
+  printHash(hash(5000));
+  std::cout << "Int: " << 100000 << " | ";
+  printHash(hash(100000));
+  std::cout << "---------------------" << std::endl;
+  std::cout << "Int: " << 5 << " | ";
+  printHash(hash(5));
+  std::cout << "---------------------" << std::endl;
+
+  // Hashing Strings with 1st function
+  std::cout << "Testing hashing strings:" << std::endl;
+  std::cout << "------------------------" << std::endl;
+  std::cout << "String: I am a COSC major" << std::endl;
+  printHash(hashTest1("I am a COSC major"));
+  std::cout << "String: Coronavirus" << std::endl;
+  printHash(hashTest1("Coronavirus"));
+  std::cout << "String: We are using zoom" << std::endl;
+  printHash(hashTest1("We are using zoom"));
+  std::cout << "------------------------" << std::endl;
+  std::cout << "String: I am a COSC major" << std::endl;
+  printHash(hashTest1("I am a COSC major"));
+  std::cout << "------------------------" << std::endl;
+
+  // Hashing Strings with 2nd function
+  std::cout << "Testing hashing strings (2):" << std::endl;
+  std::cout << "----------------------------" << std::endl;
+  std::cout << "String: Coding in the morning" << std::endl;
+  printHash(hashTest2("Coding in the morning"));
+  std::cout << "String: I enjoy COSC" << std::endl;
+  printHash(hashTest2("I enjoy COSC"));
+  std::cout << "String: Arch Linux master distribution" << std::endl;
+  printHash(hashTest2("Arch Linux master distribution"));
+  std::cout << "----------------------------" << std::endl;
+  std::cout << "String: Coding in the morning" << std::endl;
+  printHash(hashTest2("Coding in the morning"));
+  std::cout << "----------------------------" << std::endl;
+  return 0;
+}
+
 size_t hash(size_t x){
     size_t w = size_t(pow(2, 64));
     size_t p = 53;
@@ -35,35 +82,3 @@ size_t hastTest2(std::string str){
   }
   return rtn % 531;
 }
-
-int main(){
-  std::cout << "Testing hashing ints:" << std::endl;
-  std::cout << "---------------------" << std::endl;
-  std::cout << "Int: " << 5 << " | ";
-  printHash(hash(5));
-  std::cout << "Int: " << 100 << " | ";
-  printHash(hash(100));
-  std::cout << "Int: " << 5000 << " | ";
-  printHash(hash(5000));
-  std::cout << "Int: " << 100000 << " | ";
-  printHash(hash(100000));
-  std::cout << "---------------------" << std::endl;
-  std::cout << "Int: " << 5 << " | ";
-  printHash(hash(5));
-  std::cout << "---------------------" << std::endl;
-  std::cout << "Testing hashing strings:" << std::endl;
-  std::cout << "------------------------" << std::endl;
-  std::cout << "String: I am a COSC major" << std::endl;
-  printHash(hashTest1("I am a COSC major"));
-  std::cout << "String: Coronavirus" << std::endl;
-  printHash(hashTest1("Coronavirus"));
-  std::cout << "String: We are using zoom" << std::endl;
-  printHash(hashTest1("We are using zoom"));
-  std::cout << "------------------------" << std::endl;
-  std::cout << "String: I am a COSC major" << std::endl;
-  printHash(hashTest1("I am a COSC major"));
-  std::cout << "------------------------" << std::endl;
-
-  return 0;
-}
-
