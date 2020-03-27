872d4f17ab89683f5e59eea8fdf855f97b9b618e
jacobduncan00 <jacobduncan00@yahoo.com>
2020-03-27 08:25:12 -0400

    Finished

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -11,8 +11,8 @@
  for Dr. Joseph Anderson's 320 class. This code rigirously
  tests the time complexity of three different sorting algorithms:
  Bubble Sort, Insertion Sort, and Selection Sort. Along with
- time complexity being recorded, I also aggregated that data into 
- a Google sheets file and have attached images of the graphs 
+ time complexity being recorded, I also aggregated that data into
+ a Google sheets file and have attached images of the graphs
  showing trends between each of them relating to the type of sort
  and the amount of input in the array.
 */
